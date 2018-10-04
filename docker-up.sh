00ad0c660d3b57e411d958a871758d8e2ee5ba53
mrclemrkz <awesomeisme007@gmail.com>
2018-10-04 10:06:43 +0530

    Adding origanizational units

--- docker-up.sh
+++ docker-up.sh
@@ -5,3 +5,5 @@ docker-compose up --build -d
 docker exec -it openldap /usr/bin/ldapadd -Y EXTERNAL -H ldapi:// -f /home/97-wso2Person.ldif
 docker exec -it openldap /usr/bin/ldapadd -Y EXTERNAL -H ldapi:// -f /home/98-scimPerson.ldif
 docker exec -it openldap /usr/bin/ldapadd -Y EXTERNAL -H ldapi:// -f /home/99-identityPerson.ldif
+
+docker exec -it openldap /usr/bin/ldapadd -x -D 'cn=admin,dc=wso2,dc=com' -w admin -H ldapi:// -f /home/add-ou.ldif
