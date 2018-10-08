7eb8fca9d7c2e02f3dbdc45c2668552fcda0a5a0
mrclemrkz <awesomeisme007@gmail.com>
2018-10-08 10:06:38 +0530

    fix for ldap server error on ldapadd

--- docker-up.sh
+++ docker-up.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-docker-compose up --build -d
+docker-compose up -d --build
+
+sleep 20
 
 docker exec -it openldap /usr/bin/ldapadd -Y EXTERNAL -H ldapi:// -f /home/97-wso2Person.ldif
 docker exec -it openldap /usr/bin/ldapadd -Y EXTERNAL -H ldapi:// -f /home/98-scimPerson.ldif
