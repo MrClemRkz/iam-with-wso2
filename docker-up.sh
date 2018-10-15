56c0ca00fc70042b24f612cf5cc1c7c9d0d5c7dc
mrclemrkz <awesomeisme007@gmail.com>
2018-10-15 16:27:43 +0530

    optimizing the code

--- docker-up.sh
+++ docker-up.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-docker-compose up -d --build
+docker-compose up -d
 
-sleep 20
+STATUS="0"
+
+until [[ ${STATUS} == *"ok"* ]]; do
+    echo "   wating until slpd is started"
+    STATUS=`docker exec -ti openldap sh -c "service slapd status"`
+    sleep 5
+done
+
+echo "   slapd has started!"
 
 docker exec -it openldap /usr/bin/ldapadd -Y EXTERNAL -H ldapi:// -f /home/97-wso2Person.ldif
 docker exec -it openldap /usr/bin/ldapadd -Y EXTERNAL -H ldapi:// -f /home/98-scimPerson.ldif
